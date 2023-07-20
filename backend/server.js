1e6da71803afdc6220adbeb5b58dd05aeb077611
MathildaBoullenger <mathilda.boullenger@outlook.com>
2023-07-20 17:45:12 +1200

    docker files and compose yml for deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.DB_PORT || 8080;
+const PORT = process.env.SERVER_PORT || 8080;
 
 const {
     usersRoutes,
